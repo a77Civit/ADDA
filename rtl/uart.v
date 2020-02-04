f92249d7993454694c2c390175eca9c7a86d6b15
civi <16xfcheng@stu.edu.cn>
2020-02-04 17:53:36 +0800

    照着原子哥写完了通用串口发送模块，方案改为位宽不一致FIFO将FIFO输出8位数据发送到串口

--- rtl/uart.v
+++ rtl/uart.v
@@ -6,16 +6,17 @@ author ：Civi Cheng
 
 */
 module (
-    input [15:0] paralle_data,
+    input [7:0] paralle_data,
     input sysclk_12,
     input i_rest_n,
     input rdempty,//读空呈高电平发送停止；//
+    input uart_en,
     output reg tx_data
     
 );
 parameter BAUD_RATE = 115200;//默认115200可以传参数改变
 parameter CLK_FREQURENCE = 12000000;//12MHz输入时钟
-parameter TIM_CNT = CLK_FREQURENCE/BAUD_RATE;//计算每位信号发送需要的计数次数
+parameter BIT_CNT = CLK_FREQURENCE/BAUD_RATE;//计算每位信号发送需要的计数次数
 
 wire uart_en;
 wire send_sta_flg;//发送状态标志位
@@ -23,11 +24,11 @@ wire en_flg;
 wire bit_flg;//0为发送低位，1为发送高位
 reg [7:0]data_buf;
 reg [3:0]tim_cnt;
-reg [15:0]paralle_data_inputbuf;
-//reg [1:0]sb_cnt;
+reg [15:0]clk_cnt;
 reg uart_en_d0,uart_en_d1;
 
-assign uart_en = ~rdempty;
+//捕获使能信号的上升沿，配合下面的延迟两个周期
+//制造了一个时钟周期的
 assign en_flg = (~uart_en_d1) & uart_en_d0;
 //将使能信号延迟两个周期
 always@(posedge sysclk_12 or negedge i_rest_n)
@@ -49,29 +50,65 @@ always@(posedge sysclk_12 or negedge i_rest_n)
 begin
     if(!i_rest_n)
         begin
-            paralle_data_inputbuf <= 16'd0;
+            data_buf <= 16'd0;
             send_sta_flg <= 1'b0;
             sb_cnt <= 2'b0;
         end
     else
         begin
-            send_sta_flg <= 1'b1;
             if(en_flg)
                 begin
                     if(!bit_flg)//若为0则发送低位若为1则发送高位 没发送完一次取反
+                        send_sta_flg <= 1'b1;
+                        data_buf <= paralle_data;
+                end
+            else
+                begin
+                    if ((tim_cnt == 4'd9)&&(clk_cnt == BIT_CNT/2)) //发送完成并再停止位的一半结束发送过程
                         begin
-                            data_buf <= paralle_data_inputbuf[7:0];//寄存待发送的数据
-                        end
+                            tx_data <= 8'd0;
+                            send_sta_flg <= 1'b0;
+                        end 
                     else 
                         begin
-                            data_buf <= paralle_data_inputbuf[8:15];
-                            
+                            tx_data <= tx_data;//否则保持不变
+                            send_sta_flg <= send_sta_flg;
                         end
-                        
                 end
         end
 end
 
+
+//计数模块
+always@(posedge sysclk_12 or negedge i_rest_n)
+begin
+    if (!i_rest_n)
+        begin
+            tim_cnt <= 4'd0;
+            clk_cnt <= 16'd0;
+        end 
+    else 
+        begin
+            if(send_sta_flg)
+                begin
+                    if (clk_cnt < BIT_CNT-1)//每比特传输所需的时间的计数
+                        begin
+                            clk_cnt <= clk_cnt + 1'b1;
+                            tim_cnt <= tim_cnt;
+                        end 
+                    else 
+                        begin
+                            tim_cnt <= tim_cnt + 1'b1;
+                            clk_cnt <= 16'd0;
+                        end
+                end
+            else
+                begin
+                    clk_cnt <= 16'd0;
+                    tim_cnt <= 4'd0;//不在发送过程就清零
+                end
+        end
+end
 
 //具体的信号传输模块
 always@(posedge sysclk_12 or negedge i_rest_n)
@@ -103,7 +140,6 @@ begin
                     tx_data <= data_buf[7];
                 4'd9:
                     tx_data <= 1'b1;//结束位
-                    bit_flg <= ~bit_flg;
                 default:
                     endcase
                 end
